5b5b30cae5bf5f7c40fbc454a5dcba9580e5e549
Prashanth Rebala <prashanth.rebala@gmail.com>
2023-07-13 19:58:34 -0400

    mock response

---
+++ server/dummyData.py
@@ -0,0 +1,8 @@
+RESPONSE = {
+    "created": 1689290113,
+    "data": [
+        {
+            "url": "https://oaidalleapiprodscus.blob.core.windows.net/private/org-3PqyQYra9LvRBGRfWOtjNN7f/user-OQ7jGQ6LMuEE8JALwQ9A4lMI/img-NSPbUzdPogDrN5DkjhJtBhF3.png?st=2023-07-13T22%3A15%3A13Z&se=2023-07-14T00%3A15%3A13Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-07-13T21%3A32%3A32Z&ske=2023-07-14T21%3A32%3A32Z&sks=b&skv=2021-08-06&sig=xZW/E0mepBw8QwOmPHimDPjEUtPCMmfXHw2oPkHCGkc%3D"
+        }
+    ]
+}
