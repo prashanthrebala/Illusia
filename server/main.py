4df56de8a6cc39030077d76abbff1a3ca7c5528a
Prashanth Rebala <prashanth.rebala@gmail.com>
2023-07-13 21:32:44 -0400

    basic router

--- server/main.py
+++ server/main.py
@@ -1,15 +1,48 @@
-from typing import Union
+from fastapi import FastAPI, Request
+from dummyData import RESPONSE
+import requests
+from config import (
+    IMAGE_GENERATION_ENDPOINT,
+    X_RAPID_API_KEY,
+    X_RAPID_API_HOST,
+    DEFAULT_IMAGE_SIZE,
+    DEFAULT_IMAGE_COUNT,
+)
 
-from fastapi import FastAPI
+url = IMAGE_GENERATION_ENDPOINT
+
+payload = {
+    "prompt": "A cute baby sea otter",
+    "n": DEFAULT_IMAGE_COUNT,
+    "size": DEFAULT_IMAGE_SIZE
+}
+headers = {
+    "content-type": "application/json",
+    "X-RapidAPI-Key": X_RAPID_API_KEY,
+    "X-RapidAPI-Host": X_RAPID_API_HOST
+}
+
 
 app = FastAPI()
 
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def root():
+    return {"data": "Hello world"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/create")
+async def create_new_image(request: Request):
+    body = await request.json()
+    print(body["prompt"])
+    response = requests.post("url",
+                             json={"prompt": body["prompt"],  "n": DEFAULT_IMAGE_COUNT,
+                                   "size": DEFAULT_IMAGE_SIZE}, headers=headers)
+    print(response.json())
+    return "Hello"
+
+
+@app.post("/images")
+def get_images():
+    print("!@!@")
+    return RESPONSE
