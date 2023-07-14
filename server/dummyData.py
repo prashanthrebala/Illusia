d01452f93c4cc108304b76560108c0c5139ea6bb
Prashanth Rebala <prashanth.rebala@gmail.com>
2023-07-13 21:32:35 -0400

    more mock responses (oai store)

--- server/dummyData.py
+++ server/dummyData.py
@@ -3,6 +3,20 @@ RESPONSE = {
     "data": [
         {
             "url": "https://oaidalleapiprodscus.blob.core.windows.net/private/org-3PqyQYra9LvRBGRfWOtjNN7f/user-OQ7jGQ6LMuEE8JALwQ9A4lMI/img-NSPbUzdPogDrN5DkjhJtBhF3.png?st=2023-07-13T22%3A15%3A13Z&se=2023-07-14T00%3A15%3A13Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-07-13T21%3A32%3A32Z&ske=2023-07-14T21%3A32%3A32Z&sks=b&skv=2021-08-06&sig=xZW/E0mepBw8QwOmPHimDPjEUtPCMmfXHw2oPkHCGkc%3D"
+        },
+        {
+            "url": "https://oaidalleapiprodscus.blob.core.windows.net/private/org-3PqyQYra9LvRBGRfWOtjNN7f/user-OQ7jGQ6LMuEE8JALwQ9A4lMI/img-X1uuJ3IlHQV0275CrhXSXpLT.png?st=2023-07-13T23%3A51%3A20Z&se=2023-07-14T01%3A51%3A20Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-07-13T21%3A31%3A52Z&ske=2023-07-14T21%3A31%3A52Z&sks=b&skv=2021-08-06&sig=noumnCljaQu7gsrLV1LULIHwlZcrVnDcxSc/2KNtH8A%3D"
+            # Grim the reaper playing chess in a candyland
+        },
+        {
+            "url": "https://oaidalleapiprodscus.blob.core.windows.net/private/org-3PqyQYra9LvRBGRfWOtjNN7f/user-OQ7jGQ6LMuEE8JALwQ9A4lMI/img-n4E8N4qje94i2qEpsNSo3FL9.png?st=2023-07-13T23%3A53%3A13Z&se=2023-07-14T01%3A53%3A13Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-07-13T21%3A30%3A41Z&ske=2023-07-14T21%3A30%3A41Z&sks=b&skv=2021-08-06&sig=fM6LOC/rGb9e49sbIT2KZ08g660wWbM0ocC%2BsOb0TTg%3D"
+            # a vibrant and surreal cityscape with towering skyscrapers, floating islands, and neon lights illuminating the streets
+        },
+        {
+            "url": "https://oaidalleapiprodscus.blob.core.windows.net/private/org-3PqyQYra9LvRBGRfWOtjNN7f/user-OQ7jGQ6LMuEE8JALwQ9A4lMI/img-K3mw0BV4Efwo6iiYZ72CrNT1.png?st=2023-07-14T00%3A03%3A38Z&se=2023-07-14T02%3A03%3A38Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-07-13T21%3A30%3A43Z&ske=2023-07-14T21%3A30%3A43Z&sks=b&skv=2021-08-06&sig=vO8532sNjR8%2B6KKDDaRxpS5v28HMalbAQc6D3AQ0ROU%3D"
+            # Colorful fish and marine life in the deep dark ocean
+            # Modern family cast as animated characters in the desert
+            # Anime version of phineas and ferb
         }
     ]
 }
