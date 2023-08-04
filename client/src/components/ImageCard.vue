e281d832e5b47eea28802a8320a83fc40935d681
Prashanth Rebala <prashanth.rebala@gmail.com>
2023-08-04 06:18:19 -0400

    added responsive styles to imagecard

--- client/src/components/ImageCard.vue
+++ client/src/components/ImageCard.vue
@@ -10,10 +10,13 @@ const backgroundStyle = {
 </script>
 
 <template>
-	<div class="flex flex-col gap-1">
-		<div class="w-96 h-96 bg-contain rounded-md" :style="backgroundStyle" />
+	<div class="flex flex-col gap-2">
 		<div
-			class="w-96 bg-rose-900 p-4 rounded-md flex items-center justify-center font-medium text-sm leading-6 text-neutral-100"
+			class="w-full max-w-[24rem] aspect-square bg-contain rounded-md"
+			:style="backgroundStyle"
+		/>
+		<div
+			class="w-full max-w-[24rem] bg-rose-900 p-4 rounded-md flex justify-center text-center font-medium text-sm leading-6 text-neutral-100"
 		>
 			{{ description }}
 		</div>
