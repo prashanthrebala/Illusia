59d467f8a1466f8c45f5ad7a3d5d08f7af925a22
Prashanth Rebala <prashanth.rebala@gmail.com>
2023-08-07 22:11:41 -0400

    updated background to be fixed

--- client/src/Layout.vue
+++ client/src/Layout.vue
@@ -10,11 +10,18 @@ const backgroundStyle = {
 <template>
 	<div
 		:style="backgroundStyle"
-		class="w-full min-h-screen bg-no-repeat bg-cover bg-right absolute top-0 left-0 -z-10 bg-amber-300"
+		class="w-full min-h-screen bg-no-repeat bg-cover bg-right fixed top-0 left-0 -z-10"
 	/>
 	<div class="w-full flex justify-center">
 		<div class="w-full max-w-[100rem]">
 			<App />
 		</div>
+	</div>
+	<div class="w-full h-36 text-xs flex justify-end items-center p-20">
+		<a
+			href="https://www.freepik.com/free-photo/colorful-abstract-nebula-space-background_16263202.htm#query=red%20black%20background&position=9&from_view=search&track=ais"
+			>Background image by rawpixel.com</a
+		>
+		&nbsp;on Freepik
 	</div>
 </template>
